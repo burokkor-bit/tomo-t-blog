bee3283e12b0d07b3870cc5207a8a2ed5737ebe1
いしいえりか <ishiierika@ishiierikanoiMac.local>
2026-04-24 00:50:02 +0900

    initial commit: Astro blog setup for tomo-t-blog.com

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'tomo-t blog';
+export const SITE_DESCRIPTION = '元教員・4児パパが本音で発信する習い事・子育て・副業ブログ';
